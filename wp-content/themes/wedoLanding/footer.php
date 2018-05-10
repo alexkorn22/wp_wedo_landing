d849fd663e1dd70a5d8e81a4b5ac03b6e4535099
Ahmed Marzouk <AhmedMarzouk266@gmail.com>
2018-05-10 10:16:20 +0300

    footer and header

---
+++ wp-content/themes/wedoLanding/footer.php
@@ -0,0 +1,42 @@
+
+<footer>
+    <div class="container">
+        <div class="row items justify-content-center align-items-center no-gutters">
+            <div class="col-12 col-md-4 col-xl-3 item">
+                <div class="phone-footer hidden-sm-down"><p>096-845-81-83</p>
+                    <p>050-070-82-57</p>
+                    <p>061-221-02-88</p>
+                    <p>г.Запорожье</p></div>
+            </div>
+            <div class="col-12 col-md-4 col-xl-6 item">
+                <div class="logo-footer"><a href=""><img src="/wp-content/themes/wedoLanding/assets/images/logoWedo.png" alt="WeDo"></a>
+                    <p>© Copyright 2018, Все права защищены</p></div>
+            </div>
+            <div class="col-12 col-md-4 col-xl-3 item">
+                <div class="menu-footer">
+                    <ul>
+                        <li><a href="#development">Разработка интернет-магазинов</a></li>
+                        <li><a href="#integration">Интеграция с учётными системами</a></li>
+                        <li><a href="#advancement">Продвижение интернет-магазинов</a></li>
+                        <li><a href="">Вакансии</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script>function changePath(o) {
+        ~["en"].indexOf(path[1]) ? window.location.href = "/en" == o ? oldUrl : "/" : window.location.href = "/" == o ? oldUrl : o + oldUrl
+    }
+
+    var oldUrl = window.location.pathname, path = oldUrl.split("/")</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"
+        integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"
+        integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"
+        crossorigin="anonymous"></script>
+<script src="/wp-content/themes/wedoLanding/assets/js/owl.carousel.min.js"></script>
+<script src="wp-content/themes/wedoLanding/assets/js/main.js"></script><!--<script src="js/bootstrap-formhelpers.min.js"></script>--></body>
+</html>
--- wp-content/themes/wedoLanding/index.php
+++ wp-content/themes/wedoLanding/index.php
@@ -1,108 +1,5 @@
-<!doctype html>
-<html lang="ru">
-<head><!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109981378-1"></script>
-    <script>function gtag() {
-            dataLayer.push(arguments)
-        }
+<?php get_header();?>
 
-        window.dataLayer = window.dataLayer || [], gtag("js", new Date), gtag("config", "UA-109981378-1")</script>
-    <!-- Google Tag Manager -->
-    <script>!function (e, t, a, n, g) {
-            e[n] = e[n] || [], e[n].push({"gtm.start": (new Date).getTime(), event: "gtm.js"});
-            var m = t.getElementsByTagName(a)[0], r = t.createElement(a);
-            r.async = !0, r.src = "https://www.googletagmanager.com/gtm.js?id=GTM-WPHJ273", m.parentNode.insertBefore(r, m)
-        }(window, document, "script", "dataLayer")</script><!-- End Google Tag Manager --><title>Разработка
-        интернет-магазина, продвижение (seo,ppc,smm) | WeDo</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="yandex-verification" content="80567c69a37b1299">
-    <meta name="freelancehunt" content="f1392e2ffcdd730">
-    <meta name="description"
-          content="Создание интернет-магазина с нуля. Интеграция с 1С и учётными системами. SEO, контекстная реклама, продвижение в социальных сетях. Бесплатная консультация.">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
-          integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
-            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="/wp-content/themes/wedoLanding/assets/css/main.css">
-    <script src="/wp-content/themes/wedoLanding/assets/js/jquery.selectric.js"></script>
-    <script src="/wp-content/themes/wedoLanding/assets/js/bootstrap-formhelpers.min.js"></script>
-    <link rel="shortcut icon" href="/wp-content/themes/wedoLanding/assets/images/logo_fav.ico" type="image/x-icon">
-</head>
-<body><!-- Yandex.Metrika counter -->
-<script type="text/javascript">!function (e, t, a) {
-        (t[a] = t[a] || []).push(function () {
-            try {
-                t.yaCounter46972284 = new Ya.Metrika({
-                    id: 46972284,
-                    clickmap: !0,
-                    trackLinks: !0,
-                    accurateTrackBounce: !0,
-                    webvisor: !0
-                })
-            } catch (e) {
-            }
-        });
-        var c = e.getElementsByTagName("script")[0], n = e.createElement("script"), r = function () {
-            c.parentNode.insertBefore(n, c)
-        };
-        n.type = "text/javascript", n.async = !0, n.src = "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/watch.js", "[object Opera]" == t.opera ? e.addEventListener("DOMContentLoaded", r, !1) : r()
-    }(document, window, "yandex_metrika_callbacks")</script>
-<noscript>
-    <div><img src="https://mc.yandex.ru/watch/46972284" style="position:absolute;left:-9999px" alt=""></div>
-</noscript><!-- /Yandex.Metrika counter -->
-<noscript>
-    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WPHJ273" height="0" width="0"
-            style="display:none;visibility:hidden"></iframe>
-</noscript>
-<header>
-    <div class="container b-head-height">
-        <div class="parallax">
-            <div class="image-parallax man"></div>
-        </div>
-        <div class="row top-line align-items-center">
-            <div class="col-5 col-md-3 col-lg-4 col-xl-4 item">
-                <div class="logo"><a href=""><img src="/wp-content/themes/wedoLanding/assets/images/logoWedo.png" alt="WeDo"></a></div>
-            </div>
-            <div class="col-7 col-md-6 col-lg-4 col-xl-4 item">
-                <div class="phone">
-                    <div class="collapse-lang hidden-sm-up">
-                        <a class="ru" href="/"><p>RU</p></a>
-                        <a class="en" href="<?=get_theme_file_uri()?>/en/"><p>EN</p></a>
-                    </div>
-                    <p class="hidden-sm-down">+38(097)564-68-71</p>
-                    <p>+38(050)687-75-11</p>
-                    <p class="hidden-md-up">г.Запорожье</p></div>
-            </div>
-            <div class="col-9 col-md-3 col-lg-4 col-xl-4 item">
-                <div class="addres"><p class="hidden-sm-down">г.Запорожье</p>
-                    <div class="collapse-lang hidden-sm-down">
-                        <a class="ru" href="/"><p>RU</p></a>
-                        <a class="en" href="<?=get_theme_file_uri()?>/en/"><p>EN</p></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row align-items-center justify-content-end" id="menu">
-            <div class="col-12 col-md-9 col-xl-6">
-                <ul>
-                    <li><a href="#development">Разработка интернет-магазинов</a></li>
-                    <li><a href="#integration">Интеграция с учётными системами</a></li>
-                    <li><a href="#advancement">Продвижение интернет-магазинов</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="row wrap-btn align-items-center justify-content-center">
-            <div class="col-12 col-xl-4">
-                <div class="button"><a href="#" data-toggle="modal" data-target="#myModal" class="btn-pink"
-                                       data-wipe="Бесплатная консультация">Бесплатная консультация</a>
-                    <!--<a href="#" class="btn-pink b24-web-form-popup-btn-12 b24-web-form-popup-btn-6" data-wipe="Бесплатная консультация">Бесплатная консультация</a>-->
-                </div>
-            </div>
-        </div>
-    </div>
-</header>
 <section id="wedo">
     <div class="container">
         <div class="row wrap-title justify-content-end">
@@ -233,32 +130,8 @@
         </div>
     </div>
 </section>
-<footer>
-    <div class="container">
-        <div class="row items justify-content-center align-items-center no-gutters">
-            <div class="col-12 col-md-4 col-xl-3 item">
-                <div class="phone-footer hidden-sm-down"><p>096-845-81-83</p>
-                    <p>050-070-82-57</p>
-                    <p>061-221-02-88</p>
-                    <p>г.Запорожье</p></div>
-            </div>
-            <div class="col-12 col-md-4 col-xl-6 item">
-                <div class="logo-footer"><a href=""><img src="/wp-content/themes/wedoLanding/assets/images/logoWedo.png" alt="WeDo"></a>
-                    <p>© Copyright 2018, Все права защищены</p></div>
-            </div>
-            <div class="col-12 col-md-4 col-xl-3 item">
-                <div class="menu-footer">
-                    <ul>
-                        <li><a href="#development">Разработка интернет-магазинов</a></li>
-                        <li><a href="#integration">Интеграция с учётными системами</a></li>
-                        <li><a href="#advancement">Продвижение интернет-магазинов</a></li>
-                        <li><a href="">Вакансии</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer><!-- Modal -->
+
+<!-- Modal -->
 <div class="modal fade" id="myModal">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -324,25 +197,7 @@
             </div>
         </div>
     </div>
-</div><!--<div class="wrap-alert">
-
-        <div class="alert-div" id="alert">
-
-            <p>Спасибо!!! Наш специалист свяжется с вами в ближайшее время.</p>
-
-            <div class="wrap-btn">
-
-                <div class="button">
-
-                    <a href="#" id="open-close" class="btn-pink" data-wipe="ОК">ОК</a>
-
-                </div>
-
-            </div>
-
-        </div>
-
-    </div>--><!-- END Modal -->
+</div>
 <div class="modal fade" id="modal-thank" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
      aria-hidden="true">
     <div class="modal-dialog" role="document">
@@ -356,17 +211,5 @@
         </div>
     </div>
 </div>
-<script>function changePath(o) {
-        ~["en"].indexOf(path[1]) ? window.location.href = "/en" == o ? oldUrl : "/" : window.location.href = "/" == o ? oldUrl : o + oldUrl
-    }
 
-    var oldUrl = window.location.pathname, path = oldUrl.split("/")</script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"
-        integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"
-        crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"
-        integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"
-        crossorigin="anonymous"></script>
-<script src="/wp-content/themes/wedoLanding/assets/js/owl.carousel.min.js"></script>
-<script src="wp-content/themes/wedoLanding/assets/js/main.js"></script><!--<script src="js/bootstrap-formhelpers.min.js"></script>--></body>
-</html>
+<?php get_footer();?>
