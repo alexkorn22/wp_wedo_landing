13abc73273aa76ff548d5e5f97ba532c148d90b3
shavulsky <admin@artorg.com.ua>
2018-05-28 22:59:34 +0300

    добавил все статические страницы

--- www/wp-content/themes/wedoMain/content/main.php
+++ www/wp-content/themes/wedoMain/content/main.php
@@ -14,7 +14,7 @@
     
 		
 	
-	<link rel="stylesheet" href="<?=$templateUriA?>/css/main.css">
+    <link rel="stylesheet" href="<?=$templateUriA?>/css/main.css">
 	<script src="<?=$templateUriA?>/js/jquery.selectric.js"></script>
 	<script src="<?=$templateUriA?>/js/bootstrap-formhelpers.min.js"></script>
 	<link rel="shortcut icon" href="<?=$templateUriA?>/img/logo_fav.ico" type="image/x-icon">
@@ -55,13 +55,13 @@
 						            <span></span>
 						        </div>
 						        <ul class="main-menu">
-						            <li><a href="index.html">Главная</a></li>
-						            <li><a href="sozdanie-saitov.html">Создание сайтов</a></li>
-						            <li><a href="internet-magaziny.html">Создание интернет-магазинов</a></li>
-						            <li><a href="kontekstnaya-reklama.html">Контекстная реклама</a></li>
-						            <li><a href="seo.html">SEO-продвижение</a></li>
-						            <li><a href="smm.html">SMM</a></li>
-						            <li class="current_page"><a href="blog.html">Блог</a></li>
+						            <li><a href="/">Главная</a></li>
+						            <li><a href="/sozdanie-saitov/">Создание сайтов</a></li>
+						            <li><a href="/internet-magaziny/">Создание интернет-магазинов</a></li>
+						            <li><a href="/kontekstnaya-reklama/">Контекстная реклама</a></li>
+						            <li><a href="/seo/">SEO-продвижение</a></li>
+						            <li><a href="/smm/">SMM</a></li>
+						            <li class="current_page"><a href="/blog/">Блог</a></li>
 						        </ul>
 						    </nav>
 						</div>
@@ -70,17 +70,17 @@
 			<div class="row align-items-center justify-content-end" id="menu">
 					<div class="col-12 col-md-9 col-xl-6">
 						<ul>
-							<li><a href="#development">Development of online stores</a></li>
-							<li><a href="#integration">Integration with accounting systems</a></li>
-							<li><a href="#advancement">Promotion of online stores</a></li>
+							<li><a href="#development">Разработка интернет-магазинов</a></li>
+							<li><a href="#integration">Интеграция с учётными системами</a></li>
+							<li><a href="#advancement">Продвижение интернет-магазинов</a></li>
 						</ul>
 					</div>
 				</div>
 			<div class="row wrap-btn align-items-center justify-content-center">
 				<div class="col-12 col-xl-4">
 					<div class="button">
-						<a href="#" data-toggle="modal" data-target="#myModal" class="btn-pink" data-wipe="Free consultation">Free consultation</a>
-						<!--<a href="#" class="btn-pink b24-web-form-popup-btn-12 b24-web-form-popup-btn-6" data-wipe="Free consultation">Free consultation</a>-->
+						<a href="#" data-toggle="modal" data-target="#myModal" class="btn-pink" data-wipe="Бесплатная консультация">Бесплатная консультация</a>
+						<!--<a href="#" class="btn-pink b24-web-form-popup-btn-12 b24-web-form-popup-btn-6" data-wipe="Бесплатная консультация">Бесплатная консультация</a>-->
 					</div>
 				</div>
 			</div>
@@ -91,18 +91,18 @@
 			<div class="row wrap-title justify-content-end">
 				<div class="col-12 col-md-12 col-xl-11">
 					<h1>WeDo</h1>
-					<h1>development of effective systems<br>of online sales </h1>
+					<h1>разработка эффективных <br>систем онлайн продаж</h1>
 				</div>
 			</div>
 			<div class="row items">
-				<div class="col-12 col-md-4 col-xl-4 align-self-center item"><p>Since <span>2008</span> in <br>the field of IT and <br>marketing</p></div>
-				<div class="col-12 col-md-4 col-xl-4 align-self-center item"><p>More than <span>100</span><br>successful web and accounting <br>systems’ projects</p></div>
-				<div class="col-12 col-md-4 col-xl-4 align-self-center item"><p>More than <span>10</span><br>specialists working with <br>the project</p></div>
+				<div class="col-12 col-md-4 col-xl-4 align-self-center item"><p>С <span>2008</span> года <br>в сфере IT и <br>маркетинга</p></div>
+				<div class="col-12 col-md-4 col-xl-4 align-self-center item"><p>Более <span>100</span><br>успешных проектов в <br>1С и web</p></div>
+				<div class="col-12 col-md-4 col-xl-4 align-self-center item"><p>Более <span>10</span><br>специалистов для <br>работы над проектом</p></div>
 			</div>
 			<div class="row wrap-btn align-items-center justify-content-center">
 				<div class="col-12 col-xl-4">
 					<div class="button">
-						<a href="#" data-toggle="modal" data-target="#myModal" class="btn-pink" data-wipe="Free consultation">Free consultation</a>
+						<a href="#" data-toggle="modal" data-target="#myModal" class="btn-pink" data-wipe="Бесплатная консультация">Бесплатная консультация</a>
 					</div>
 				</div>
 			</div>
@@ -112,8 +112,8 @@
 		<div class="container">
 			<div class="row wrap-title justify-content-end">
 				<div class="col-12 col-md-12 col-xl-11">
-					<h2>Development of online stores</h2>
-					<h2>we will help you to sell products 24 hours <br class='hidden-sm-down'> a day, 7 days a week</h2>
+					<h2>Разработка интернет-магазинов</h2>
+					<h2>мы поможем вам продавать товары 24 <br class='hidden-sm-down'> часа в сутки 7 дней в неделю</h2>
 				</div>
 			</div>
 
@@ -121,12 +121,12 @@
 				<div class="col-12 col-md-12 col-lg-6 col-xl-6">
 					<section class="timeline">
 						<ul>
-							<li><div>Analysis <br>of business <br>and niche</div></li>
-							<li><div>Strategy <br>development</div></li>
-							<li><div>Website <br>design and <br>programming</div></li>
-							<li><div>Integration <br>with accounting <br>systems</div></li>
-							<li><div>The launch of <br>the project and <br>technical support</div></li>
-							<li><div>Marketing <br>promotion</div></li>
+							<li><div>Анализ <br>бизнеса <br>и ниши</div></li>
+							<li><div>Разработка <br>стратегии</div></li>
+							<li><div>Проектирование <br>и дизайн <br>сайта</div></li>
+							<li><div>Интеграция <br>с учётными <br>системами</div></li>
+							<li><div>Запуск <br>проекта и <br>техподдержка</div></li>
+							<li><div>Маркетинговое <br>продвижение</div></li>
 						</ul>
 					</section>
 				</div>
@@ -135,17 +135,17 @@
 						<div class="ribbon-text">
 							<div class="wrap-thesis">
 								<ul class="thesis">
-									<li>Mobile Friendly</li>
-									<li>UX design</li>
-									<li>SEO, PPC, SMM</li>
-									<li>High conversion rate</li>
-									<li>Prompt tech support</li>
+									<li>Мобильная адаптация</li>
+									<li>Продающий дизайн</li>
+									<li>SEO оптимизация</li>
+									<li>Высокая конверсия</li>
+									<li>Оперативная техподдержка</li>
 								</ul>
 							</div>
 						</div>
 						<div class="wrap-btn">
 							<div class="button">
-								<a href="#" data-toggle="modal" data-target="#myModal" class="btn-pink" data-wipe="Online store calculation">Online store calculation</a>
+								<a href="#" data-toggle="modal" data-target="#myModal" class="btn-pink" data-wipe="Расчёт интернет-магазина">Расчёт интернет-магазина</a>
 							</div>
 						</div>
 					</div>
@@ -157,18 +157,18 @@
 		<div class="container">
 			<div class="row wrap-title justify-content-end">
 				<div class="col-12 col-md-12 col-xl-11">
-					<h2>Integration with accounting systems</h2>
-					<h2>we will accelerate the acceptance of orders and provide actual product prices and availability on the website</h2>
+					<h2>Интеграция с учётными системами</h2>
+					<h2>мы ускорим приём заказов и обеспечим <br class='hidden-sm-down'> актуальные цены и наличие на сайте</h2>
 					<!--<h2>мы ускорим приём заказов и обеспечим<br class="hidden-sm-down">актуальные цены и наличие на сайте</h2>-->
 				</div>
 			</div>
 			<div class="row items">
-				<div class="col-12 col-md-6 col-xl-6 align-self-center item"><p>Automatic <br><span>uploading</span><br> of products and orders</p></div>
-				<div class="col-12 col-md-6 col-xl-6 align-self-center item"><p>Automated <br><span>Manager’s workplace</span><br> for dealing with the orders</p></div>
+				<div class="col-12 col-md-6 col-xl-6 align-self-center item"><p>Автоматическая <br><span>загрузка-выгрузка</span><br> для обработки заказа</p></div>
+				<div class="col-12 col-md-6 col-xl-6 align-self-center item"><p>Автоматизированное <br><span>рабочее место менеджера</span><br> товаров и заказов</p></div>
 			</div>
 			<div class="row items">
-				<div class="col-12 col-md-6 col-xl-6 align-self-center item"><p>The system for <span> Customer <br>relationship management</span><br> (CRM)</p></div>
-				<div class="col-12 col-md-6 col-xl-6 align-self-center item"><p>Complex <br><span>discount and bonus</span><br> programs in the online shop</p></div>
+				<div class="col-12 col-md-6 col-xl-6 align-self-center item"><p>Системы управления <br><span>отношениями с клиентами</span><br> (CRM)</p></div>
+				<div class="col-12 col-md-6 col-xl-6 align-self-center item"><p>Внедрение сложных <br><span>дисконтных и бонусных</span><br> программ в онлайн магазине</p></div>
 			</div>
 		</div>
 	</section>
@@ -176,34 +176,34 @@
 		<div class="container">
 			<div class="row wrap-title justify-content-end">
 				<div class="col-12 col-md-12 col-xl-11">
-					<h2>The promotion of an online store</h2>
-					<h2>we bring targeted customers and increase <br>brand awareness</h2>
+					<h2>Продвижение интернет-магазина</h2>
+					<h2>мы приведем на сайт целевых покупателей <br>и повысим узнаваемость бренда</h2>
 				</div>
 			</div>
 			<div class="row items justify-content-center no-gutters">
 				<div class="col-12 col-md-6 col-xl-4 align-self-center item">
 					<a href="/seo/" class="link-to-other-page" target="_blank">
 						<p>SEO</p>
-						<p>Increases online <br>stores’ visibility in search <br>engines (Google, Bing, <br>Yandex etc)</p>
+						<p>Увеличение видимости <br>интернет-магазина в <br>поисковой системе и <br>привлечение пользователей</p>
 					</a>
 				</div>
 				<div class="col-12 col-md-6 col-xl-4 align-self-center item">
 					<a href="/kontekstnaya-reklama/" class="link-to-other-page" target="_blank">
 						<p>PPC</p>
-						<p>Quickly attracts <br>customers that plan to <br>purchase online stores’ <br>goods of defined kind</p>
+						<p>Контекстная реклама <br>для быстрого привлечения <br>клиентов, планирующих <br>покупку</p>
 					</a>
 				</div>
 				<div class="col-12 col-md-6 col-xl-11 align-self-center item hidden-sm-down">
 					<div class="wrap-btn">
 						<div class="button">
-							<a href="#" data-toggle="modal" data-target="#myModal" class="btn-pink" data-wipe="Help in choosing">Help in choosing</a>
+							<a href="#" data-toggle="modal" data-target="#myModal" class="btn-pink" data-wipe="Что выбрать Вам?">Что выбрать Вам?</a>
 						</div>
 					</div>
 				</div>
 				<div class="col-12 col-md-6 col-xl-4 align-self-center item">
 					<a href="/smm/" class="link-to-other-page" target="_blank">
 						<p>SMM</p>
-						<p>Increases company’s <br>and brand’s awareness <br>by gaining customers’ <br>confidence</p>
+						<p>Повышения узнаваемости <br>компании и бренда, <br>завоевания доверия <br>потенциальных клиентов</p>
 					</a>
 				</div>
 			</div>
@@ -211,7 +211,7 @@
 				<div class="col-12 col-xl-11">
 					<div class="wrap-btn">
 						<div class="button">
-							<a href="#" data-toggle="modal" data-target="#myModal" class="btn-pink" data-wipe="Help in choosing">Help in choosing</a>
+							<a href="#" data-toggle="modal" data-target="#myModal" class="btn-pink" data-wipe="Что выбрать Вам?">Что выбрать Вам?</a>
 						</div>
 					</div>
 				</div>
@@ -224,22 +224,22 @@
 				<div class="col-12 col-md-4 col-xl-3 item">
 					<div class="phone-footer hidden-sm-down">
 						<p>+38(098)150-83-95</p>
-						<p>Zaporozhye, Ukraine</p>
+						<p>г.Запорожье</p>
 					</div>
 				</div>
 				<div class="col-12 col-md-4 col-xl-6 item">
 					<div class="logo-footer">
 						<a href=""><img src="<?=$templateUriA?>/img/logoWedo.png" alt="WeDo"></a>
-						<p>© Copyright 2017, All rights reserved</p>
+						<p>© Copyright 2018, Все права защищены</p>
 					</div>
 				</div>
 				<div class="col-12 col-md-4 col-xl-3 item">
 					<div class="menu-footer">
 						<ul>
-							<li><a href="#development">Development of online stores</a></li>
-							<li><a href="#integration">Integration with accounting systems</a></li>
-							<li><a href="#advancement">Promotion of online stores</a></li>
-							<li><a href="">Job opportunity</a></li>
+							<li><a href="#development">Разработка интернет-магазинов</a></li>
+							<li><a href="#integration">Интеграция с учётными системами</a></li>
+							<li><a href="#advancement">Продвижение интернет-магазинов</a></li>
+							<li><a href="">Вакансии</a></li>
 						</ul>
 					</div>
 				</div>
@@ -260,7 +260,7 @@
 				</div>
 				<div class="row justify-content-center">
 					<div class="col-10">
-						<h3 class="modal-title" id="exampleModalLabel">Please enter your contact details and we will provide you with a free consultation</h3>
+						<h3 class="modal-title" id="exampleModalLabel">Укажите, пожалуйста, Ваши контактные данные, и мы предоставим Вам бесплатную консультацию</h3>
 					</div>
 				</div>
 				<div id="hidden-row" class="row">
@@ -270,28 +270,28 @@
 								<form id="callback-form">
 									<div class="input-container">
 										<input type="text" id="Username" name="Username" required>
-										<label for="Username">Name</label>
+										<label for="Username">Имя</label>
 										<div class="bar"></div>
 									</div>
 									<div class="input-container number">
 										<input class="form-control bfh-phone" type="text" id="phone" value="" name="phone" data-format="+380 (dd) ddd-dd-dd" required>
-										<label for="phone">Phone</label>
+										<label for="phone">Телефон</label>
 										<div class="bar"></div>
 									</div>
-									<div class="errorMessage" style="display: none">Specify your phone. (see the hint)</div>
+									<div class="errorMessage" style="display: none">Укажите Ваш телефон. (см. подсказку)</div>
 									<div class="input-container">
 										<select class="form-control" id="select" name="select" required>
-											<option value="" disabled selected>You need</option>
-											<option value="Development of online stores">Development of online stores</option>
-											<option value="Integration with accounting systems">Integration with accounting systems</option>
-											<option value="Promotion of online stores">Promotion of online stores</option>
+											<option value="" disabled selected>Вам требуется</option>
+											<option value="Разработка интернет-магазина">Разработка интернет-магазина</option>
+											<option value="Интеграция с учётными системами">Интеграция с учётными системами</option>
+											<option value="Продвижение интернет-магазина">Продвижение интернет-магазина</option>
 										</select>
 										<div class="bar"></div>
 									</div>
 									<div class="button-container">
 										<div class="wrap-btn">
 											<div class="button">
-												<button type="submit" class="btn" data-wipe="Send">Send</button>
+												<button type="submit" class="btn" data-wipe="Отправить заявку">Отправить заявку</button>
 												<!--<a href="" id="submit-btn" class="btn-pink" data-wipe="Отправить заявку">Отправить заявку</a>-->
 											</div>
 										</div>
@@ -307,7 +307,7 @@
 	</div>
     <!--<div class="wrap-alert">
         <div class="alert-div" id="alert">
-            <p>Thank you! We will contact you to discuss the project.</p>
+            <p>Спасибо!!! Наш специалист свяжется с вами в ближайшее время.</p>
             <div class="wrap-btn">
                 <div class="button">
                     <a href="#" id="open-close" class="btn-pink" data-wipe="ОК">ОК</a>
@@ -320,7 +320,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-body">
-                    <p>Thank you! We will contact you to discuss the project.</p>
+                    <p>Спасибо!!! Наш специалист свяжется с вами в ближайшее время.</p>
                 </div>
                 <div class="modal-footer">
                     <div class="wrap-btn">
