84019d431e1b08a2f60a574ea76dd06cf1ff7735
Aleksandr <kornsasha89@gmail.com>
2019-01-04 10:39:19 +0200

    https://trello.com/c/ewhFn2MF АРТ-О Перенос верстки на wordpress.

--- www/wp-content/themes/wedoMain/header.php
+++ www/wp-content/themes/wedoMain/header.php
@@ -1,15 +1,10 @@
 <!doctype html>
-<html lang="ru">
+<html <?php language_attributes(); ?>>
 <head>
-    <title>Разработка интернет-магазина, продвижение (seo,ppc,smm) | WeDo</title>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="yandex-verification" content="80567c69a37b1299" />
-    <meta name='freelancehunt' content='f1392e2ffcdd730' />
-    <meta name="description" content="Создание интернет-магазина с нуля. Интеграция с 1С и учётными системами. SEO, контекстная реклама, продвижение в социальных сетях. Бесплатная консультация.">
-
     <link rel="shortcut icon" href="<?=App::$app->pathAsset?>/img/logo_fav.ico" type="image/x-icon">
 
     <?wp_head()?>
